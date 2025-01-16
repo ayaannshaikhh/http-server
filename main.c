8af8430b122912667506be76a6ca49959ee41851
Ayaan Shaikh <ayaannshaikhh8@gmail.com>
2025-01-16 14:54:23 -0500

    Fixed typo and added second header

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 void launch(struct Server *server) {
     char buffer[30000];
-    char *hello = "HTTP/1.1 200 OK\nGMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed 22 Jul 2009 19:15:56 GMT\nContet-Type: text/html\nConnection: Closed\n\n<html><body><h1>Hello, World!</h1></body></html>";
+    char *hello = "HTTP/1.1 200 OK\nGMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed 22 Jul 2009 19:15:56 GMT\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Hello, World!</h1><h2>My name is Ayaan!</h2></body></html>";
     int address_length = sizeof(server->address); 
     int new_socket;
     while (1) {
